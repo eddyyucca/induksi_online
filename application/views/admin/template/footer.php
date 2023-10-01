10d751b2ef0a735164972266f8278d46bc276764
eddyyucca <eddyyucca@gmail.com>
2023-10-01 15:58:47 +0800

    update

---
+++ application/views/admin/template/footer.php
@@ -0,0 +1,63 @@
+     <!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+            <div class="copyright">
+               <p>Copyright © Developed by <a href="https://dexignzone.com/" target="_blank">DexignZone</a> 2023</p>
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
+
+		<!--**********************************
+           Support ticket button start
+        ***********************************-->
+		
+        <!--**********************************
+           Support ticket button end
+        ***********************************-->
+
+
+	</div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+     <script src="<?= base_url("vendor/") ?>vendor/global/global.min.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/chart.js/Chart.bundle.min.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/apexchart/apexchart.js"></script>
+	
+	<!-- Dashboard 1 -->
+	<script src="<?= base_url("vendor/") ?>js/dashboard/dashboard-1.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/draggable/draggable.js"></script>
+	
+	
+	<!-- tagify -->
+	<script src="<?= base_url("vendor/") ?>vendor/tagify/dist/tagify.js"></script>
+	 
+	<script src="<?= base_url("vendor/") ?>vendor/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/datatables/js/dataTables.buttons.min.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/datatables/js/buttons.html5.min.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/datatables/js/jszip.min.js"></script>
+	<script src="<?= base_url("vendor/") ?>js/plugins-init/datatables.init.js"></script>
+   
+	<!-- Apex Chart -->
+	
+	<script src="<?= base_url("vendor/") ?>vendor/bootstrap-datetimepicker/js/moment.js"></script>
+	<script src="<?= base_url("vendor/") ?>vendor/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+	
+
+	<!-- Vectormap -->
+    <script src="<?= base_url("vendor/") ?>vendor/jqvmap/js/jquery.vmap.min.js"></script>
+    <script src="<?= base_url("vendor/") ?>vendor/jqvmap/js/jquery.vmap.world.js"></script>
+    <script src="<?= base_url("vendor/") ?>vendor/jqvmap/js/jquery.vmap.usa.js"></script>
+     <script src="<?= base_url("vendor/") ?>js/custom.js"></script>
+	<script src="<?= base_url("vendor/") ?>js/deznav-init.js"></script>
+</body>
+</html>
